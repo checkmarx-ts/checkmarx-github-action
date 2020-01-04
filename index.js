87ba9a47dda3facfe519df4e517bcde145d753c7
Miguel Freitas <13312380+miguelfreitas93@users.noreply.github.com>
2020-01-04 19:01:33 +0000

    update node script

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const exec = require('@actions/exec');
 async function run() {
     try {
         core.info('Event name: ' + github.context.eventName);
+
         //GET Inputs
         let cxServer = core.getInput('cxServer');
 		let cxUsername = core.getInput('cxUsername');
@@ -14,6 +15,15 @@ async function run() {
 		let cxHigh = core.getInput('cxHigh');
 		let cxMedium = core.getInput('cxMedium');
         let cxLow = core.getInput('cxLow');
+        let cxComment = core.getInput('cxComment');
+        let cxForceScan = core.getInput('cxForceScan');
+        let cxIncremental = core.getInput('cxIncremental');
+        let cxExcludeFolders = core.getInput('cxExcludeFolders');
+        let cxExcludeFiles = core.getInput('cxExcludeFiles');
+        let cxConfiguration = core.getInput('cxConfiguration');
+        let cxPrivate = core.getInput('cxPrivate');
+        let cxLog = core.getInput('cxLog');
+        let cxVerbose = core.getInput('cxVerbose');
         
         core.info('cxServer: ' + cxServer);
         core.info('cxUsername: ' + cxUsername);
@@ -22,6 +32,15 @@ async function run() {
         core.info('cxHigh: ' + cxHigh);
         core.info('cxMedium: ' + cxMedium);
         core.info('cxLow: ' + cxLow);
+        core.info('cxComment: ' + cxComment);
+        core.info('cxForceScan: ' + cxForceScan);
+        core.info('cxIncremental: ' + cxIncremental);
+        core.info('cxExcludeFolders: ' + cxExcludeFolders);
+        core.info('cxExcludeFiles: ' + cxExcludeFiles);
+        core.info('cxConfiguration: ' + cxConfiguration);
+        core.info('cxPrivate: ' + cxPrivate);
+        core.info('cxLog: ' + cxLog);
+        core.info('cxVerbose: ' + cxVerbose);
 
     } catch (error) {
 		core.setFailed(error.message);
