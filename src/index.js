18dc0bdb02103218ae7e4db5c195dd8a69542dec
Miguel Freitas <13312380+miguelfreitas93@users.noreply.github.com>
2020-01-05 23:42:01 +0000

    no message

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cxsast = require('./cxsast.js')
 const cxosa = require('./cxosa.js')
 const core = require('@actions/core')
 const github = require('@actions/github')
+const envs = process.env
 let action = "Scan"
 let version = "8.9"
 let server
@@ -13,10 +14,10 @@ let verbose = true
 async function run() {
     try {
         core.info('Event name: ' + github.context.eventName)
-        core.info('Repository : ' + GITHUB_REPOSITORY)
-        core.info('Branch : ' + GITHUB_REF)
-        core.info('Workspace : ' + GITHUB_WORKSPACE)
-        core.info('Commit SHA : ' + GITHUB_SHA)
+        core.info('Repository : ' + envs.GITHUB_REPOSITORY)
+        core.info('Branch : ' + envs.GITHUB_REF)
+        core.info('Workspace : ' + envs.GITHUB_WORKSPACE)
+        core.info('Commit SHA : ' + envs.GITHUB_SHA)
 
         core.info("\n[START] Read Inputs...")
 
