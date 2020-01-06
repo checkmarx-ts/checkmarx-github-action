3384686c5a062e08b535bdfae4f6d74d424b260a
Miguel Freitas <13312380+miguelfreitas93@users.noreply.github.com>
2020-01-06 11:08:57 +0000

    no message

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -29,47 +29,5 @@ describe('index', function () {
             let cmd = await index.run()
             assert(!cmd)
         })
-        it('Valid Action, Valid Server Test - Fail', async function () {
-            process.env["INPUT_CXACTION"] = "test"
-            process.env["INPUT_CXSERVER"] = server
-            let cmd = await index.run()
-            assert(!cmd)
-        })
-        it('Valid Action, Valid Server 0 - Fail', async function () {
-            process.env["INPUT_CXACTION"] = VALID_ACTIONS[0]
-            process.env["INPUT_CXSERVER"] = server
-            let cmd = await index.run()
-            assert(!cmd)
-        })
-        it('Valid Action, Valid Server 1 - Fail', async function () {
-            process.env["INPUT_CXACTION"] = VALID_ACTIONS[1]
-            process.env["INPUT_CXSERVER"] = server
-            let cmd = await index.run()
-            assert(!cmd)
-        })
-        it('Valid Action, Valid Server 2 - Fail', async function () {
-            process.env["INPUT_CXACTION"] = VALID_ACTIONS[2]
-            process.env["INPUT_CXSERVER"] = server
-            let cmd = await index.run()
-            assert(!cmd)
-        })
-        it('Valid Action, Valid Server 3 - Fail', async function () {
-            process.env["INPUT_CXACTION"] = VALID_ACTIONS[3]
-            process.env["INPUT_CXSERVER"] = server
-            let cmd = await index.run()
-            assert(!cmd)
-        })
-        it('Valid Action, Valid Server 4 - Fail', async function () {
-            process.env["INPUT_CXACTION"] = VALID_ACTIONS[4]
-            process.env["INPUT_CXSERVER"] = server
-            let cmd = await index.run()
-            assert(!cmd)
-        })
-        it('Valid Action, Valid Server 5 - Fail', async function () {
-            process.env["INPUT_CXACTION"] = VALID_ACTIONS[5]
-            process.env["INPUT_CXSERVER"] = server
-            let cmd = await index.run()
-            assert(!cmd)
-        })
     })
 })
